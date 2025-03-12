2d0e11a64efbf930526c9002019ec96b1d1c01e6
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-03-12 16:13:29 +0200

    fix versions.tf

--- task03/versions.tf
+++ task03/versions.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.22.0"
     }
   }
 }
 
 provider "azurerm" {
-      features {}
+  features {}
 }
 
