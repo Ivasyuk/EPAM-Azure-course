9bf7e899d4853c0311aa056cccf0ac9b72db1149
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-03-13 16:13:22 +0200

    fix variables use tfvars

---
+++ task04/terraform.tfvars
@@ -0,0 +1,20 @@
+location = "East US"
+
+vm_admin_username = "azureuser"
+
+# Set this manually or provide it during `terraform apply`
+vm_password = "YourSecureP@ssword123"
+
+resource_group_name = "cmaz-efe625c7-mod4-rg"
+vnet_name           = "cmaz-efe625c7-mod4-vnet"
+subnet_name         = "frontend"
+nsg_name            = "cmaz-efe625c7-mod4-nsg"
+nic_name            = "cmaz-efe625c7-mod4-nic"
+public_ip_name      = "cmaz-efe625c7-mod4-pip"
+dns_label           = "cmaz-efe625c7-mod4-nginx"
+vm_name             = "cmaz-efe625c7-mod4-vm"
+vm_size             = "Standard_F2s_v2"
+
+tags = {
+  Creator = "ruslan_ivasiuk@epam.com"
+}
--- task04/variables.tf
+++ task04/variables.tf
@@ -1,13 +1,11 @@
 variable "location" {
   description = "Azure region"
   type        = string
-  default     = "East US"
 }
 
 variable "vm_admin_username" {
   description = "Admin username for the VM"
   type        = string
-  default     = "azureuser"
 }
 
 variable "vm_password" {
@@ -19,61 +17,49 @@ variable "vm_password" {
 variable "resource_group_name" {
   description = "Resource Group Name"
   type        = string
-  default     = "cmaz-efe625c7-mod4-rg"
 }
 
 variable "vnet_name" {
   description = "Virtual Network Name"
   type        = string
-  default     = "cmaz-efe625c7-mod4-vnet"
 }
 
 variable "subnet_name" {
   description = "Subnet Name"
   type        = string
-  default     = "frontend"
 }
 
 variable "nsg_name" {
   description = "Network Security Group Name"
   type        = string
-  default     = "cmaz-efe625c7-mod4-nsg"
 }
 
 variable "nic_name" {
   description = "Network Interface Name"
   type        = string
-  default     = "cmaz-efe625c7-mod4-nic"
 }
 
 variable "public_ip_name" {
   description = "Public IP Name"
   type        = string
-  default     = "cmaz-efe625c7-mod4-pip"
 }
 
 variable "dns_label" {
   description = "DNS label for the public IP"
   type        = string
-  default     = "cmaz-efe625c7-mod4-nginx"
 }
 
 variable "vm_name" {
   description = "Virtual Machine Name"
   type        = string
-  default     = "cmaz-efe625c7-mod4-vm"
 }
 
 variable "vm_size" {
   description = "VM Size"
   type        = string
-  default     = "Standard_F2s_v2"
 }
 
 variable "tags" {
   description = "Tags for resources"
   type        = map(string)
-  default = {
-    Creator = "ruslan_ivasiuk@epam.com"
-  }
 }
