80f664b9fda90734a87c0377d6fa564735aabd54
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-03-15 19:59:20 +0200

    fix version

--- task04/main.tf
+++ task04/main.tf
@@ -102,7 +102,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
+    sku       = "24_04-server"
     version   = "latest"
   }
 
