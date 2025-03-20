db7d3cef1b36828f1cc1367752c1c0d8f9cc540c
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-03-20 10:30:45 +0200

    implemet for each

--- task05/main.tf
+++ task05/main.tf
@@ -1,5 +1,5 @@
 
-module "rg1" {
+/* module "rg1" {
   source              = "./modules/resource_group"
   resource_group_name = var.resource_groups.rg1.name
   location            = var.resource_groups.rg1.location
@@ -18,13 +18,24 @@ module "rg3" {
   resource_group_name = var.resource_groups.rg3.name
   location            = var.resource_groups.rg3.location
   tags                = var.tags
+} */
+
+module "resource_groups" {
+  source = "./modules/resource_group"
+
+  for_each = var.resource_groups
+
+  resource_group_name = each.value.name
+  location            = each.value.location
+  tags                = var.tags
 }
+
 
 module "asp1" {
   source              = "./modules/app_service_plan"
   name                = var.app_service_plans.asp1.name
-  resource_group_name = module.rg1.resource_group_name
-  location            = module.rg1.resource_group_location
+  resource_group_name = module.resource_groups["rg1"].resource_group_name
+  location            = module.resource_groups["rg1"].location
   sku_tier            = var.app_service_plans.asp1.sku_tier
   sku_size            = var.app_service_plans.asp1.sku_size
   worker_count        = var.app_service_plans.asp1.worker_count
@@ -34,8 +45,8 @@ module "asp1" {
 module "asp2" {
   source              = "./modules/app_service_plan"
   name                = var.app_service_plans.asp2.name
-  resource_group_name = module.rg2.resource_group_name
-  location            = module.rg2.resource_group_location
+  resource_group_name = module.resource_groups["rg2"].resource_group_name
+  location            = module.resource_groups["rg2"].location
   sku_tier            = var.app_service_plans.asp2.sku_tier
   sku_size            = var.app_service_plans.asp2.sku_size
   worker_count        = var.app_service_plans.asp2.worker_count
@@ -45,8 +56,8 @@ module "asp2" {
 module "app1" {
   source                = "./modules/app_service"
   name                  = var.app_services.app1.name
-  resource_group_name   = module.rg1.resource_group_name
-  location              = module.rg1.resource_group_location
+  resource_group_name   = module.resource_groups["rg1"].resource_group_name
+  location              = module.resource_groups["rg1"].location
   app_service_plan_id   = module.asp1.asp_id
   verification_agent_ip = var.verification_ip
   tags                  = var.tags
@@ -55,8 +66,8 @@ module "app1" {
 module "app2" {
   source                = "./modules/app_service"
   name                  = var.app_services.app2.name
-  resource_group_name   = module.rg2.resource_group_name
-  location              = module.rg2.resource_group_location
+  resource_group_name   = module.resource_groups["rg2"].resource_group_name
+  location              = module.resource_groups["rg2"].location
   app_service_plan_id   = module.asp2.asp_id
   verification_agent_ip = var.verification_ip
   tags                  = var.tags
@@ -66,8 +77,8 @@ module "app2" {
 module "traffic_manager" {
   source              = "./modules/traffic_manager"
   name                = var.tm_profile_name
-  resource_group_name = module.rg3.resource_group_name
-  location            = module.rg3.resource_group_location
+  resource_group_name = module.resource_groups["rg3"].resource_group_name
+  location            = module.resource_groups["rg3"].location
   app1_id             = module.app1.app_id
   app2_id             = module.app2.app_id
   tags                = var.tags
