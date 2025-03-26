a377be9314bbdd32eb945d8c24310b70d518b4ae
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-03-26 15:11:18 +0200

    add resorce group

---
+++ task06/terraform.tfvars
@@ -0,0 +1,8 @@
+
+resource_group_name = "cmaz-efe625c7-mod6-kv-rg"
+
+resource_group_location = "East US"
+
+tags = {
+    Creator = "ruslan_ivasiuk@epam.com"
+}
---
+++ task06/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "Resource Group Name"
+  type        = string
+}
+
+variable "tags" {
+  description = "Resource Group Name"
+  type        = map(string)
+}
