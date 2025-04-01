7d4cdc37b76fea2dea5e6a5480621d0544fa116d
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-01 18:44:21 +0300

    add description

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,6 +1,7 @@
 output "sql_connection_string" {
   value = "Server=${azurerm_sql_server.example.fully_qualified_domain_name};Database=${azurerm_sql_database.example.name};User Id=${azurerm_sql_server.example.administrator_login};Password=${data.azurerm_key_vault_secret.sql_password.value};Encrypt=true;Connection Timeout=30;"
   sensitive = true
+  description = "value of the SQL connection string"
 }
 output "sql_server_fqdn" {
   description = "The fully qualified domain name (FQDN) of the SQL Server"
