0ea1f6d5f2137d71dc22c66cf50eecfe57ac92a6
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-01 20:58:09 +0300

    fix output

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -2,8 +2,6 @@ output "sql_connection_string" {
   sensitive   = true
   description = "The connection string for the SQL Database"
   value       = "Server=tcp:{server_name},1433;Initial Catalog={db_name};Persist Security Info=False;User ID={sql_user_name};Password={sql_user_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-
-
 }
 output "sql_server_fqdn" {
   description = "The fully qualified domain name (FQDN) of the SQL Server"
