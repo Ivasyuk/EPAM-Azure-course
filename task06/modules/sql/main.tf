ae281036e0aa2948fdfb552c39ba4bdfb7110d68
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-01 14:12:36 +0300

    fix issue

--- task06/modules/sql/main.tf
+++ task06/modules/sql/main.tf
@@ -20,7 +20,7 @@ resource "random_password" "sql_admin" {
 }
 
 
-resource "azurerm_sql_database" "this" {
+resource "azurerm_sql_database" "db" {
   name                             = var.database_name
   resource_group_name              = var.resource_group_name
   location                         = var.location
