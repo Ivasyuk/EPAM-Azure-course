5d7e20875f38a5045bc7af66ae9c66c452785d09
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-01 23:54:32 +0300

    1001 fix

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,10 +1,12 @@
 output "sql_connection_string" {
   sensitive   = true
   description = "The connection string for the SQL Database"
-  value       = "Server=tcp:${azurerm_mssql_server.server.name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${azurerm_mssql_server.server.administrator_login};Password=${azurerm_mssql_server.server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  value       = "Server=tcp:{${azurerm_mssql_server.server.name}},1433;Initial Catalog={${azurerm_mssql_database.db.name}};Persist Security Info=False;User ID={${azurerm_mssql_server.server.administrator_login}};Password={${azurerm_mssql_server.server.administrator_login_password}};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
 output "sql_server_fqdn" {
   description = "The fully qualified domain name (FQDN) of the SQL Server"
   value       = azurerm_mssql_server.server.fully_qualified_domain_name
 }
+
+// Server=tcp:{server_name},1433;Initial Catalog={db_name};Persist Security Info=False;User ID={sql_user_name};Password={sql_user_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 
