59edb5166b9bcd55549e23fe5a6b1cf9154aacd3
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-01 17:54:13 +0300

    fix fmt

--- task06/modules/sql/main.tf
+++ task06/modules/sql/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_mssql_server" "server" {
   version                      = "12.0"
   administrator_login          = var.admin_username
   administrator_login_password = azurerm_key_vault_secret.sql_admin_password.value
-  
+
   tags = var.tags
 }
 
