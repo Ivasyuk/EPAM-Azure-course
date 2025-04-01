00d530b6ad9d3663325b63f2619956102b06606f
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-01 18:21:18 +0300

    fix fmt

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,5 +1,5 @@
 output "sql_connection_string" {
-    value = format(
+  value = format(
     "Server=tcp:%s,1433;Initial Catalog=%s;Persist Security Info=False;User ID=%s;Password=%s;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
     azurerm_mssql_server.server.fully_qualified_domain_name,
     azurerm_mssql_database.db.name,
