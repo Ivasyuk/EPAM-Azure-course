041fd919111d8bbb8657eda642fe2fcb45dfd70b
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-01 18:20:46 +0300

    change sql connection string

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,5 +1,11 @@
 output "sql_connection_string" {
-  value       = "Server=tcp:${azurerm_mssql_server.server.fully_qualified_domain_name},1433; Database=${azurerm_sql_database.db.name};  User ID=${var.admin_username};  Password=${azurerm_key_vault_secret.sql_admin_password.value};"
+    value = format(
+    "Server=tcp:%s,1433;Initial Catalog=%s;Persist Security Info=False;User ID=%s;Password=%s;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
+    azurerm_mssql_server.server.fully_qualified_domain_name,
+    azurerm_mssql_database.db.name,
+    var.admin_username,
+    azurerm_key_vault_secret.sql_admin_password.value
+  )
   sensitive   = true
   description = "The connection string for the SQL Database in ADO.NET format"
 
