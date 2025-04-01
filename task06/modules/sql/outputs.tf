edac3ad3826c2284ac3190c71553826b47e03fa1
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-01 14:55:03 +0300

    fix mssql

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,8 +1,8 @@
 output "sql_connection_string" {
   description = "The connection string for the SQL Database in ADO.NET format"
   value = format("Server=tcp:%s.database.windows.net,1433;Initial Catalog=%s;User ID=%s;Password=%s;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
-    azurerm_mysql_server.server.name,
-    azurerm_mysql_database.db.name,
+    azurerm_mssql_server.server.name,
+    azurerm_mssql_database.db.name,
     var.admin_username,
     random_password.sql_admin.result
   )
