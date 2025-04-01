3f2f43cc7135e5254561009214d296e1e30b5cae
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-01 23:43:40 +0300

    fixi

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,7 +1,7 @@
 output "sql_connection_string" {
   sensitive   = true
   description = "The connection string for the SQL Database"
-  value       = "Server=tcp:${azurerm_mssql_server.server.name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${azurerm_mssql_server.server.administrator_login};Password=${azurerm_key_vault_secret.sql_admin_password.value};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  value       = "Server=tcp:${azurerm_mssql_server.server.name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${azurerm_mssql_server.server.administrator_login};Password=${azurerm_mssql_server.server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
 output "sql_server_fqdn" {
   description = "The fully qualified domain name (FQDN) of the SQL Server"
