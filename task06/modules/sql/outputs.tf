22b701d3d9574b7d5b8e7ae996b2c264b03dd96f
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-01 18:01:10 +0300

    fix sensitive

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,7 +1,8 @@
 output "sql_connection_string" {
-  description = "The connection string for the SQL Database in ADO.NET format"
   value       = "Server=tcp:${azurerm_mssql_server.server.fully_qualified_domain_name},1433; Database=${azurerm_sql_database.db.name};  User ID=${var.admin_username};  Password=${azurerm_key_vault_secret.sql_admin_password.value};"
   sensitive   = true
+  description = "The connection string for the SQL Database in ADO.NET format"
+
 }
 output "sql_server_fqdn" {
   description = "The fully qualified domain name (FQDN) of the SQL Server"
