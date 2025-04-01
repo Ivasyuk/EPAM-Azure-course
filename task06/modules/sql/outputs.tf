7e4dd976c21fdcfd971319481f745fb8f8785c88
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-01 19:12:36 +0300

    fix stirng

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,6 +1,6 @@
 output "sql_connection_string" {
-  value       = "Server=${azurerm_sql_server.example.fully_qualified_domain_name};Database=${azurerm_sql_database.example.name};User Id=${azurerm_sql_server.example.administrator_login};Password=${data.azurerm_key_vault_secret.sql_password.value};Encrypt=true;Connection Timeout=30;"
-  sensitive   = true
+  value = sensitive("Server=tcp:${azurerm_mssql_server.server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${azurerm_mssql_server.server.administrator_login};Password=${azurerm_mssql_server.server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${azurerm_mssql_server.server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+  sensitive = true
   description = "value of the SQL connection string"
 }
 output "sql_server_fqdn" {
