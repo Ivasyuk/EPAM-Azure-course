0bd8506e05610ecec28796edbeb58647a9c19afd
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-02 12:26:06 +0300

    fix string

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -6,7 +6,7 @@ output "sql_connection_string" {
                 Initial Catalog=${azurerm_mssql_database.db.name};
                 Persist Security Info=False;User ID=${azurerm_mssql_server.server.administrator_login};
                 Password=${azurerm_mssql_server.server.administrator_login_password};
-                MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+                MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
                 EOT
 }
 
