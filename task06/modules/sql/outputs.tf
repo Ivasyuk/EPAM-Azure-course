6ef5616f856e543383daa005ee751faf3718c73d
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-02 11:41:39 +0300

    change sql connection string

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,7 +1,13 @@
 output "sql_connection_string" {
   sensitive   = true
   description = "The connection string for the SQL Database"
-  value       = "Server=tcp:${azurerm_mssql_server.server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${azurerm_mssql_server.server.administrator_login};Password=${azurerm_mssql_server.server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  value       = <<-EOT
+                Server=tcp:${azurerm_mssql_server.server.name},1433;
+                Initial Catalog=${azurerm_mssql_database.db.name};
+                Persist Security Info=False;User ID=${azurerm_mssql_server.server.administrator_login};
+                Password=${azurerm_mssql_server.server.administrator_login_password};
+                MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+                EOT
 }
 
 output "sql_server_fqdn" {
