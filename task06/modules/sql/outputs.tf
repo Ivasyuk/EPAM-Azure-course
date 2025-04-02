88056496166cf1c667c8d468bc19eb48fc77526a
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-02 12:26:29 +0300

    remove comment

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -14,6 +14,3 @@ output "sql_server_fqdn" {
   description = "The fully qualified domain name (FQDN) of the SQL Server"
   value       = azurerm_mssql_server.server.fully_qualified_domain_name
 }
-
-// Server=tcp:{server_name},1433;Initial Catalog={db_name};Persist Security Info=False;User ID={sql_user_name};Password={sql_user_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-
