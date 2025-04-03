0c2eb058da00cf6789a74e4e5435fd1c8820eb9d
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-04 00:18:46 +0300

    fix service plan

--- task06/modules/webapp/main.tf
+++ task06/modules/webapp/main.tf
@@ -12,9 +12,9 @@ resource "azurerm_app_service_plan" "asp" {
 
 resource "azurerm_linux_web_app" "webapp" {
   name                = var.webapp_name
-  location            = azurerm_service_plan.asp.location
+  location            = azurerm_app_service_plan.asp.location
   resource_group_name = var.resource_group_name
-  service_plan_id     = azurerm_service_plan.asp.id
+  service_plan_id     = azurerm_app_service_plan.asp.id
 
   site_config {
     application_stack {
