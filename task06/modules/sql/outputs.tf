c8b750464aefb03c9106f39e6ced4295bb2a907d
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-03 12:15:19 +0300

    change output format

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,13 +1,14 @@
 output "sql_connection_string" {
   description = "SQL connection string"
   sensitive   = true
-  value = format(
-    "Server=tcp:%s.database.windows.net,1433;Initial Catalog=%s;Persist Security Info=False;User ID=%s;Password=%s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
-    azurerm_mssql_server.server.name,
-    azurerm_mssql_database.db.name,
-    var.admin_username,
-    random_password.sql_admin.result
-  )
+  value = <<-EOT
+    "Server=tcp:${azurerm_mssql_server.server.name}.database.windows.net,1433;
+    Initial Catalog=${azurerm_mssql_database.db.name};
+    Persist Security Info=False;
+    User ID=${var.admin_username};
+    Password=${random_password.sql_admin.result};
+    MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  EOT
 }
 
 output "sql_server_fqdn" {
