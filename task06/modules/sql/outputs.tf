102007a24b870e3c3a94d453725a70e052311719
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-04-03 16:20:58 +0300

    fix

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -4,6 +4,7 @@ output "sql_connection_string" {
   value       = "Server=tcp:${azurerm_mssql_server.server.name}.database.windows.net,1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${azurerm_mssql_server.server.administrator_login};Password=${azurerm_mssql_server.server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
 
+
 output "sql_server_fqdn" {
   description = "The fully qualified domain name (FQDN) of the SQL Server"
   value       = azurerm_mssql_server.server.fully_qualified_domain_name
