c66964753349ddbb8ac51123648b27b7586709ed
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-05-15 14:28:54 +0300

    fix

--- task08/modules/aci/outputs.tf
+++ task08/modules/aci/outputs.tf
@@ -1,4 +1,4 @@
-output "aci_fqdn" {
-  description = "Fully Qualified Domain Name for the Azure Container Instance"
+output "fqdn" {
+  description = "The FQDN of the container group derived from `dns_name_label`."
   value       = azurerm_container_group.container_tas08.fqdn
 }
