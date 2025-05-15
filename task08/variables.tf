a3e62afce3b2450b37c5a65884c94f230a6dffa7
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-05-15 13:59:49 +0300

    add name prefix to variables

--- task08/variables.tf
+++ task08/variables.tf
@@ -14,10 +14,12 @@ variable "tags" {
 }
 
 variable "name_prefix" {
+  description = "value name prefix"
   type = string
 }
 
 variable "name_prefix_cr" {
+  default = "value name prefix for cr"
   type = string
 }
 
