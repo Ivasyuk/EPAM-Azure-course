0708ed17411e47d67d3f8e7a4da8fc9118655d96
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-05-17 20:51:23 +0300

    fix acr

--- task08/modules/acr/main.tf
+++ task08/modules/acr/main.tf
@@ -1,34 +1,50 @@
 resource "azurerm_container_registry" "acr" {
   name                = var.name
   location            = var.location
-  resource_group_name = var.resource_group
+  resource_group_name = var.resource_group_name
   sku                 = var.sku
-  admin_enabled       = true
+  admin_enabled       = false
   tags                = var.tags
 }
 
 resource "azurerm_container_registry_task" "build" {
   name                  = "build-task"
   container_registry_id = azurerm_container_registry.acr.id
-  enabled               = true
-  timeout_in_seconds    = 3600
-  is_system_task        = false
-  tags                  = var.tags
 
   platform {
-    os = "Linux"
+    os           = "Linux"
+    architecture = "amd64"
   }
 
   docker_step {
-    context_path         = "https://github.com/Ivasyuk/EPAM-Azure-course#main:task08/"
     dockerfile_path      = "application/Dockerfile"
-    image_names          = [var.image_name]
-    push_enabled         = true
-    cache_enabled        = true
+    context_path         = var.git_repo_url
     context_access_token = var.git_pat
+    image_names = [
+      "${azurerm_container_registry.acr.login_server}/${var.image_repo_name}:latest"
+    ]
+    push_enabled  = true
+    cache_enabled = true
   }
+
+  source_trigger {
+    name           = "github-trigger"
+    source_type    = "Github"
+    repository_url = var.git_repo_url
+    branch         = var.git_repo_branch
+    events         = ["commit"]
+
+    authentication {
+      token         = var.git_pat
+      token_type    = "PAT"
+      scope         = "repo"
+      refresh_token = null
+    }
+  }
+
+  tags = var.tags
 }
 
-resource "azurerm_container_registry_task_schedule_run_now" "example" {
+resource "azurerm_container_registry_task_schedule_run_now" "schedule_run" {
   container_registry_task_id = azurerm_container_registry_task.build.id
 }
