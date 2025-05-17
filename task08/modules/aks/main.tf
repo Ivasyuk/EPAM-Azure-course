bcb85fa29122b017a0cefc222a2e281fb271d780
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-05-17 20:24:59 +0300

    fix aks

--- task08/modules/aks/main.tf
+++ task08/modules/aks/main.tf
@@ -1,25 +1,46 @@
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.name
   location            = var.location
-  resource_group_name = var.resource_group
-  dns_prefix          = var.name
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.dns_prefix
 
   default_node_pool {
-    name         = "system"
-    node_count   = var.node_count
-    vm_size      = var.node_size
-    os_disk_type = "Managed"
+    name            = var.node_pool_name
+    node_count      = var.node_count
+    vm_size         = var.node_vm_size
+    os_disk_size_gb = 30
+    os_disk_type    = "Ephemeral"
   }
 
   identity {
     type = "SystemAssigned"
   }
 
+  workload_identity_enabled = true
+
   key_vault_secrets_provider {
-    secret_rotation_enabled = false
+    secret_rotation_enabled = true
   }
+
+  oidc_issuer_enabled = true
 
   tags = var.tags
 }
 
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = var.acr_id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
 
+output "kube_config" {
+  description = "AKS kube_config block"
+  value = {
+    host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+    client_certificate     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+    client_key             = azurerm_kubernetes_cluster.aks.kube_config[0].client_key
+    cluster_ca_certificate = azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
+    username               = azurerm_kubernetes_cluster.aks.kube_config[0].username
+    password               = azurerm_kubernetes_cluster.aks.kube_config[0].password
+  }
+}
--- task08/modules/aks/outputs.tf
+++ task08/modules/aks/outputs.tf
@@ -1,24 +1,28 @@
+output "kubelet_identity" {
+  description = "The kubelet identity block with client_id and object_id"
+  value       = azurerm_kubernetes_cluster.aks.kubelet_identity
+}
+
+output "kube_config_raw" {
+  description = "Raw kube config for kubectl provider"
+  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
+
 output "host" {
-  value = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.host
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+  value     = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "client_key" {
-  value = azurerm_kubernetes_cluster.aks.kube_config[0].client_key
+  value     = azurerm_kubernetes_cluster.aks.kube_config.0.client_key
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
-}
-
-
-output "name" {
-  value = azurerm_kubernetes_cluster.aks.name
-}
-
-output "id" {
-  value = azurerm_kubernetes_cluster.aks.id
+  value     = azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate
+  sensitive = true
 }
