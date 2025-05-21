268f0d08ad076cee094ec47d2a73c3ac8d11f450
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-05-21 21:48:47 +0300

    add aci redis module

---
+++ task08_b/modules/aci_redis/main.tf
@@ -0,0 +1,60 @@
+resource "random_password" "redis_password" {
+  length           = 24 
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?" 
+  upper            = true
+  lower            = true
+  numeric          = true
+}
+
+resource "azurerm_container_group" "redis_ci" {
+  name                = var.aci_redis_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  ip_address_type     = "Public" 
+  os_type             = "Linux"
+  sku                 = var.aci_sku
+  tags                = var.tags
+  dns_name_label      = "${var.aci_redis_name}-dns"
+  container {
+    name = "redis"
+    
+    image  = "mcr.microsoft.com/cbl-mariner/base/redis:6.2" 
+    cpu    = 1.0                                            
+    memory = 1.5                                            
+    ports {
+      port     = 6379
+      protocol = "TCP"
+    }
+
+    commands = [
+      "redis-server",
+      "--requirepass", random_password.redis_password.result,
+      "--protected-mode", "no"
+    ]
+  }
+
+}
+
+resource "time_sleep" "wait_for_redis" {
+  depends_on      = [azurerm_container_group.redis_ci]
+  create_duration = "2m"
+}
+
+resource "azurerm_key_vault_secret" "redis_hostname" {
+  name         = var.redis_hostname_secret_name
+  value        = azurerm_container_group.redis_ci.fqdn 
+  key_vault_id = var.key_vault_id
+
+  tags       = var.tags
+  depends_on = [time_sleep.wait_for_redis] 
+}
+
+resource "azurerm_key_vault_secret" "redis_password" {
+  name         = var.redis_password_secret_name
+  value        = random_password.redis_password.result
+  key_vault_id = var.key_vault_id
+
+  tags       = var.tags
+  depends_on = [time_sleep.wait_for_redis]
+}
