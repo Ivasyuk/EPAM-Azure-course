90dd68211af1e1e2155a1f103aa26f6dcb9c6e40
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-05-21 21:58:29 +0300

    add kubernetes module

---
+++ task08_b/modules/k8s/main.tf
@@ -0,0 +1,56 @@
+
+resource "kubectl_manifest" "secret_provider_class" {
+
+
+  yaml_body = templatefile("${path.module}/../../k8s-manifests/secret-provider.yaml.tftpl", {
+    kv_name                    = var.key_vault_name
+    redis_url_secret_name      = var.redis_hostname_secret_name_in_kv
+    redis_password_secret_name = var.redis_password_secret_name_in_kv
+    tenant_id                  = var.tenant_id
+    aks_kv_access_identity_id  = var.aks_kv_identity_client_id
+  })
+}
+
+resource "kubectl_manifest" "service" {
+
+  yaml_body = file("${path.module}/../../k8s-manifests/service.yaml")
+
+  depends_on = [
+    kubectl_manifest.secret_provider_class
+  ]
+}
+
+resource "kubectl_manifest" "deployment" {
+
+  yaml_body = templatefile("${path.module}/../../k8s-manifests/deployment.yaml.tftpl", {
+    acr_login_server = var.acr_login_server
+    app_image_name   = var.app_image_name
+    image_tag        = var.image_tag
+  })
+
+  wait_for_rollout = false
+
+  depends_on = [
+    kubectl_manifest.service
+  ]
+}
+
+resource "time_sleep" "wait_for_k8s_lb_ip" {
+  create_duration = "7m" # Or your preferred duration
+
+  depends_on = [
+    kubectl_manifest.service 
+  ]
+}
+
+data "kubernetes_service" "app_service_data" {
+
+  metadata {
+    name      = "redis-flask-app-service"
+    namespace = "default"
+  }
+
+  depends_on = [
+    time_sleep.wait_for_k8s_lb_ip
+  ]
+}
