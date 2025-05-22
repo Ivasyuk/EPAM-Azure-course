3dff2056a9c1dbbb9498059770f020a11b472288
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-05-22 14:46:28 +0300

    add outputs

--- task09/modules/afw/outputs.tf
+++ task09/modules/afw/outputs.tf
@@ -1,14 +1,9 @@
-output "azure_firewall_public_ip" {
-  description = "Azure Firewall Public IP address"
-  value       = azurerm_public_ip.firewall_pip.ip_address
+output "firewall_public_ip" {
+  description = "The public IP address of the Azure Firewall"
+  value       = azurerm_public_ip.firewall.ip_address
 }
 
-output "azure_firewall_private_ip" {
-  description = "Azure Firewall Private IP address"
-  value       = azurerm_firewall.firewall.ip_configuration[0].private_ip_address
-}
-
-output "route_table_id" {
-  description = "ID of the created Route Table"
-  value       = azurerm_route_table.route_table.id
+output "firewall_private_ip" {
+  description = "The private IP address of the Azure Firewall"
+  value       = azurerm_firewall.this.ip_configuration[0].private_ip_address
 }
