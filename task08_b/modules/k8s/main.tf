6c222d3c0ff644a85f5a5d27db70e2976cea4791
Ruslan Ivasiuk <ruslan_ivasiuk@epam.com>
2025-05-22 12:38:03 +0300

    change IP address

--- task08_b/modules/k8s/main.tf
+++ task08_b/modules/k8s/main.tf
@@ -36,7 +36,7 @@ resource "kubectl_manifest" "deployment" {
 }
 
 resource "time_sleep" "wait_for_k8s_lb_ip" {
-  create_duration = "5m"
+  create_duration = "7m"
 
   depends_on = [
     kubectl_manifest.service
